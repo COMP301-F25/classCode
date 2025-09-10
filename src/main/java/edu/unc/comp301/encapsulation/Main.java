183b067c78201daa1f53abb0363b9df9f49fed47
prairie <prg@cs.unc.edu>
2025-09-10 06:38:05 -0400

    final triangle example

--- src/main/java/edu/unc/comp301/encapsulation/Main.java
+++ src/main/java/edu/unc/comp301/encapsulation/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
         DoubleTriangle tri = new DoubleTriangle(0,0,1,2,2,0);
         DoubleTriangle tri2 = new DoubleTriangle(2,0,3,2,4,0);
+        System.out.println(tri);
+        System.out.println(tri2);
 
         Point a = new Point(0,0);
         Point b = new Point(1,2);
